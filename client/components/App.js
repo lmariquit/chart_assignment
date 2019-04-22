5ef0cb60e7ad7074b9d1e0b393e37e8b8baa05ce
Loren Mariquit <l.mariquit@gmail.com>
2019-04-22 14:50:02 -0400

    removed script tag causing issues.

--- client/components/App.js
+++ client/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import SockJS from 'sockjs-client'
 var Stomp = require('stompjs')
 
 import CanvasJSReact from '../../public/assets/canvasjs.react'

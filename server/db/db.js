8226c6af558a750a418caf4176bcae045c824189
Loren Mariquit <l.mariquit@gmail.com>
2019-04-22 14:15:33 -0400

    added database name to sequelize instance

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,13 @@
 const Sequelize = require('sequelize')
+const pkg = require('../../package.json')
 
-const db = new Sequelize('postgres://localhost:5432/exOneQuotes', {
-  logging: false
-})
+const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  {
+    logging: false
+  }
+)
 
 module.exports = db
